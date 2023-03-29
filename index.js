028377153632b56fbbb6a9626b73f445a68188e3
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-03-29 15:24:42 -0400

    added how to read a document

--- index.js
+++ index.js
@@ -14,15 +14,29 @@ initializeApp({
 const db = getFirestore();
 
 //  add a product to our products collection
-const candy = {
-    name: "Skittles",
-    UnitPrice: 3.99,
-    size: "16 oz",
-    color: "green",
-    inventory: 144,
-    productNumber: 7
+const candy2 = {
+    name: "Twix",
+    UnitPrice: 2.99,
+    size: "12 oz",
+    color: "gold",
+    inventory: 288,
+    productNumber: 2
 }
 
-db.collection('products').add(candy)
-    .then(doc => console.log("added doc: " + doc.id))
+//How to add a document to firestore
+// db.collection('products').add(candy2)
+//     .then((doc) => {
+//         console.log("added doc: " + doc.id)
+//     })
+//     .catch(err => console.log(err))
+
+
+//How to read a document from Firestore:
+db.collection('products').doc('fGAuwNTN2IpcDBISTVYa').get()
+    .then(doc => {
+        console.log(doc.data())
+    })
     .catch(err => console.log(err))
+    //.catch(console.log)  the above line does the same
+
+
